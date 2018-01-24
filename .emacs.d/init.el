105faa964b96817f786c5cc33030f1041cc89c38
Naoki Nakajima <n2.jima@gmail.com>
2018-01-25 05:22:36 +0900

    update emacs settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,42 +1,41 @@
+;;; init.el -- Emacs Configuration
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
+             (lambda ()
+               '("melpa-stable" . "https://stable.melpa.org/packages/") t
+               '("melpa" . "https://melpa.org/packages/") t))
+(add-to-list 'custom-theme-load-path "~/.emacs.d/atom-one-dark-theme/")
 
 ;;=======================
 ;; load
 ;;=======================
 ;; load cask
+;;(package-initialize)
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
-;; ui
-(load-file "~/.emacs.d/interface.el")
-
-;; keybindings
-(load-file "~/.emacs.d/keybindings.el")
-
-;; load path recursive
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
-        (add-to-list 'load-path default-directory)
-        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-            (normal-top-level-add-subdirs-to-load-path))))))
-;;(add-to-load-path "elpa")
-;;(add-to-load-path "~/.emacs.d/.cask")
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (edit-indirect ein auto-virtualenv virtualenvwrapper yasnippet web-mode use-package smex smartrep smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit lua-mode idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell emmet-mode drag-stuff auto-complete))))
 
 ;;=======================
-;; Basic settings
+;; misc settings
 ;;=======================
-
 ;; ignore low level warnings
 (setq warning-minimum-level :emergency)
+
+;; scroll
+(setq scroll-conservatively 1)
 
 ;; encoding
 (set-locale-environment nil)
@@ -80,16 +79,93 @@
 ;; add final new line if no line
 (setq require-final-newline t)
 
+;;=======================
+;; Appearance
+;;=======================
+;;load theme
+(if (display-graphic-p)
+    (progn (load-theme 'atom-one-dark t)) ; x11
+  (load-theme 'wombat t)) ; console
+
+;; line/column number mode
+(global-linum-mode t)
+(column-number-mode t)
+
+;; highlight current line
+(global-hl-line-mode t)
+
+;; highlight color of region
+(set-face-background 'region "#565864")
+
+;; highlight parenthese
+(show-paren-mode t)
+(rainbow-delimiters-mode-enable)
+
+;; hide menu-bar, toolbar and scroll bar
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode -1)
+
+;; helm color
+(require 'helm)
+(set-face-attribute 'helm-selection nil
+                    :background "lightgreen"
+                    :foreground "black")
+
+;; font settings
+(set-face-attribute 'default nil :family "Ricty" :height 125)
+(set-fontset-font t 'unicode (font-spec :family "Noto Emoji"))
+
+;; web-mode bracket color
+(add-hook 'web-mode-hook
+          '(lambda ()
+             (set-face-attribute 'web-mode-html-tag-face nil :foreground "#4A8ACA")
+             (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "#87CEEB")
+             (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "#FFFFFF")
+             (set-face-attribute 'web-mode-css-at-rule-face nil :foreground "#DFCF44")
+             (set-face-attribute 'web-mode-comment-face nil :foreground "#587F35")
+             (set-face-attribute 'web-mode-css-selector-face nil :foreground "#DFCF44")
+             (set-face-attribute 'web-mode-css-pseudo-class-face nil :foreground "#DFCF44")
+             (set-face-attribute 'web-mode-css-property-name-face nil :foreground "#87CEEB")
+             (set-face-attribute 'web-mode-css-string-face nil :foreground "#D78181")))
+
+;; indent-guide
+(require 'indent-guide)
+(indent-guide-global-mode)
+(set-face-background 'indent-guide-face "color-200")
+(setq indent-guide-delay 0)
+(setq indent-guide-char "|")
+
+;; powerline
+(require 'powerline)
+(powerline-center-theme)
+
+;;=======================
+;; Global Keybinds
+;;=======================
+;; C-h to BackSpace
+(global-set-key [?\C-h] 'delete-backward-char)
+
+;; flycheck
+(require 'flycheck)
+(global-set-key (kbd "M-p") 'flycheck-previous-error)
+(global-set-key (kbd "M-n") 'flycheck-next-error)
+
+;;=======================
+;; popwin
+;;=======================
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
 
 ;;=======================
 ;; multiple-cursors
 ;;=======================
 (require 'multiple-cursors)
-(require 'smartrep)
 (global-set-key (kbd "C-M-c") 'mc/edit-lines)
 (global-set-key (kbd "C-M-r") 'mc/mark-all-in-region)
 (global-unset-key "\C-t")
 
+(require 'smartrep)
 (smartrep-define-key global-map "C-t"
   '(("C-t" . 'mc/mark-next-like-this)
     ("n"   . 'mc/mark-next-like-this)
@@ -104,37 +180,29 @@
     ("o"   . 'mc/sort-regions)
     ("O"   . 'mc/reverse-regions)))
 
-
 ;;=======================
 ;; flycheck
 ;;=======================
 (require 'flycheck)
-
-;; global flycheck mode
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq-default flycheck-disabled-checkers '(python-flake8))
 (setq flycheck-check-syntax-automatically '(mode-enabled save))
 
-
 ;;=======================
-;; auto-complete
+;; company-mode
 ;;=======================
-(require 'auto-complete-config)
-(ac-config-default)
-(setq ac-auto-show-menu 0.8)
-(add-to-list 'ac-modes 'text-mode)
-(add-to-list 'ac-modes 'fundamental-mode)
-(add-to-list 'ac-modes 'org-mode)
-(add-to-list 'ac-modes 'python-mode)
-
+(require 'company)
+(global-company-mode +1)
+(setq company-minimum-prefix-length 1)
+(setq company-selection-wrap-around t)
 
 ;;=======================
 ;; helm
 ;;=======================
+(require 'helm)
 (helm-mode t)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
 
 ;;=======================
 ;; PHP
@@ -144,47 +212,28 @@
             (setq tab-width 4)
             (setq c-basic-offset 4)))
 
-
 ;;=======================
 ;; Python
 ;;=======================
-;; virtualenvwrapper
 (require 'virtualenvwrapper)
-(setq venv-location '("/home/inab/python/rogue_like/env"
-                      "/home/inab/python/syncdrive"
-                      "/home/inab/workspace/Positioning"))
-
-;(require 'epc)
-(require 'python)
-(jedi:setup)
-(setq jedi:complete-on-dot t)
-(add-to-list 'ac-sources 'ac-source-filename)
-(add-to-list 'ac-sources 'ac-source-jedi-direct)
-
-
+(require 'auto-virtualenvwrapper)
+(add-hook 'python-mode-hook #'auto-virtualenvwrapper-activate)
+(add-to-list 'company-backends 'company-jedi)
 (add-hook 'python-mode-hook
           '(lambda()
-            ;(flycheck-mode 1)
             (setq flycheck-checker 'python-pylint
                   flycheck-checker-error-threshold 900
-                  ;flycheck-python-pylint-executable (getenv "PYLINT_EXECUTABLE")
                   flycheck-pylintrc "~/.pylintrc")
-            (setq-default show-trailing-whitespace t)
-            #'auto-virtualenvwrapper-activate))
-
-;; quickrun
-(setq-default quickrun-timeout-seconds nil)
-(require 'popwin)
-(push '("*quickrun*" :stick t) popwin:special-display-config)
-
+            'jedi:setup
+            (setq-default show-trailing-whitespace t)))
 
 ;;=======================
 ;; HTML, CSS
 ;;=======================
-(require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tag\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 
 ;; emmet settings
 (require 'emmet-mode)
@@ -194,17 +243,57 @@
           (lambda()
             (setq web-mode-markup-indent-offset 2)
             (emmet-mode t)))
+
+;;=======================
+;; Rust
+;;=======================
+(require 'rust-mode)
+(require 'racer)
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
+(eval-after-load "rust-mode"
+  '(setq-default rust-format-on-save t))
+(add-hook 'rust-mode-hook (lambda ()
+                           (racer-mode)
+                           (flycheck-rust-setup)))
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook (lambda ()
+                             (set (make-variable-buffer-local 'company-idle-delay) 0.5)
+                             (set (make-variable-buffer-local 'company-minimum-prefix-length) 2)))
+
+;;=======================
+;; neotree
+;;=======================
+(require 'all-the-icons)
+(require 'neotree)
+(setq neo-show-hidden-files t)
+(setq neo-create-file-auto-open t)
+(setq neo-persist-show t)
+(setq neo-keymap-style 'concise)
+(setq neo-smart-open t)
+(global-set-key [f8] 'neotree-toggle)
+(when neo-persist-show
+  (add-hook 'popwin:before-popup-hook
+            (lambda () (setq neo-persist-show nil)))
+  (add-hook 'popwin:after-popup-hook
+            (lambda () (setq neo-persist-show t))))
+(setq neo-theme (if (display-graphic-p) 'icons 'arrows))
+
+;;=======================
+;; shell pop
+;;=======================
+(require 'shell-pop)
+(add-hook 'term-mode-hook (lambda () (setq show-trailing-whitespace nil)))
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ein auto-virtualenv virtualenvwrapper yasnippet web-mode use-package smex smartrep smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit lua-mode idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell emmet-mode drag-stuff auto-complete))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(shell-pop-shell-type (quote ("ansi-term" "*shell-pop-ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-term-shell "/bin/zsh")
+ '(shell-pop-universal-key "C-`")
+ '(shell-pop-window-height 40)
+ '(shell-pop-window-position "bottom"))
+
+;;=======================
+;; windmove
+;;=======================
+(setq windmove-wrap-around t)
+(windmove-default-keybindings)
+
+;;; init.el ends here
