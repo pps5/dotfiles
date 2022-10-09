73ea7c08e50748f241b5595f4f74f4e75375fa42
Naoki Nakajima <n2.jima@gmail.com>
2022-10-09 23:32:54 +0900

    Delete manjaro configs

--- manjaro/.i3/battery.sh
+++
@@ -1,6 +0,0 @@
-set $(acpi | sed -e 's/^.*\(Charging\|Discharging\|Unknown\), \(.*\)%.*$/\1 \2/')
-echo ${0} ${1} ${2}
-if [ "${1}" = "Discharging" -a ${2} -le 10 ]; then
-    notify-send -u critical -t 6000 "Battery low: ${2}%"
-fi
-
--- manjaro/.i3/time_signal.sh
+++
@@ -1,4 +0,0 @@
-RUNNING_NYANCAT=$(pgrep nyancat > /dev/null; echo $?)
-if [ $RUNNING_NYANCAT -eq 1 ]; then
-    exec termite -e "nyancat" --class nyancat &
-fi
